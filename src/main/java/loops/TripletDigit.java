f3568360d3217659b7db81b49076f5b11d9348f7
DIPALI RAJPUT <maherdipali@gmail.com>
2021-05-29 10:58:51 +0530

    triplet pattern

--- src/main/java/loops/TripletDigit.java
+++ src/main/java/loops/TripletDigit.java
@@ -1,15 +1,16 @@
 package loops;
 
 public class TripletDigit {
-    static void printTripletDigit(int[]array){
-        for (int i = 0; i < array.length ; i++) {
-            for (int j =0; j < array.length ; j++) {
-                for (int k =0; k < array.length ; k++) {
-                   //if (i==!j&&j==!k)
-                    //  continue;
-                    System.out.printf("i= %d j= %d k= %d\n", i, j, k);
-                    System.out.println(" ");
-                    System.out.printf("%d -> %d -> %d",array[i],array[j],array[k]);
+    static void printTripletDigit(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                for (int k = 0; k < array.length; k++) {
+                    // if (i==j||j==k||k==i)
+                    //continue;
+                    if (i != j && j != k && k != i) {
+                        System.out.printf("\ni= %d j= %d k= %d\n", i, j, k);
+                        System.out.printf("%d -> %d -> %d", array[i], array[j], array[k]);
+                    }
                 }
 
             }
@@ -18,7 +19,7 @@ public class TripletDigit {
     }
 
     public static void main(String[] args) {
-        int arr[]={1,2,3};
+        int arr[] = {1, 2, 3};
         printTripletDigit(arr);
     }
 }

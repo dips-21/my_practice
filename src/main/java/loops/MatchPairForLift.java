ed02c2a3f7e0a259a8d615c67595d2124ed5068a
DIPALI RAJPUT <maherdipali@gmail.com>
2021-05-30 15:56:41 +0530

    Count total number of matches of male female on the basis of weight to enter in lift.

---
+++ src/main/java/loops/MatchPairForLift.java
@@ -0,0 +1,30 @@
+package loops;
+
+public class MatchPairForLift {
+    static int matchPairForLiftEntry(int weightOfmale[], int weightOfFemale[], String MaleNames[], String femaleNames[]) {
+        int count = 0;
+        for (int i = 0; i < weightOfmale.length; i++)
+           {
+                int sum = weightOfmale[i] + weightOfFemale[i];
+                if (sum == 46) {
+                    System.out.println("you can enter");
+                    count++;
+                    System.out.println(MaleNames[i]+"->"+femaleNames[i]);
+                }
+                else
+                {
+                    System.out.println("you cannot enter");
+                    System.out.println(MaleNames[i]+"->"+femaleNames[i]);
+                }
+            }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String maleNames[] = {"Harshal", "Vedant", " Akshay", "Prasad", "Jayant"};
+        String femaleNames[] = {"Sanjana", "Trushali", "Jaanhvi", "Vishakha", "Smita"};
+        int weightOfMale[] = {25, 35, 36, 29, 27};
+        int weightOfFemale[] = {21, 24, 35, 36, 19};
+        matchPairForLiftEntry(weightOfMale,weightOfFemale,maleNames,femaleNames);
+    }
+}

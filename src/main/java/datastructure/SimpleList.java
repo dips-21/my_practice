56ea68b28a0e6ccb20247ff264ffe474d9e5693c
DIPALI RAJPUT <maherdipali@gmail.com>
2021-06-29 20:13:03 +0530

    Nth from last node

--- src/main/java/datastructure/SimpleList.java
+++ src/main/java/datastructure/SimpleList.java
@@ -3,7 +3,8 @@ package datastructure;
 public interface SimpleList<T> {
     void add(T element) ;
     T removeLast();
-    T removeLast(int position);
+    T remove(int position);
+    T remove(T element);
     T get(int position);
     void add(int position,T element);
     Iterator<T> getIterator();
--- src/test/java/datastructure/SimpleLinkedListTest.java
+++ src/test/java/datastructure/SimpleLinkedListTest.java
@@ -2,9 +2,11 @@ package datastructure;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import sunbeamPractical.Singer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+
 
 public class SimpleLinkedListTest {
     //10--20--30
@@ -20,6 +22,19 @@ public class SimpleLinkedListTest {
         assertThat(simpleLinkedList.get(2), is(30));
         assertThat(simpleLinkedList.get(3), is(40));
 
+    }
+    @Test
+    public void getNthFromLast() {
+        SimpleLinkedList<Integer> simpleLinkedList = new SimpleLinkedList<>();
+        simpleLinkedList.add(10);
+        simpleLinkedList.add(20);
+        simpleLinkedList.add(30);
+        simpleLinkedList.add(40);
+        simpleLinkedList.add(50);
+        simpleLinkedList.add(60);
+
+        assertThat(simpleLinkedList.getNthNodeFromLast(3).data, is(40));
+        assertThat(simpleLinkedList.getNthNodeFromLast(1).data, is(60));
     }
 
     @Test
@@ -51,7 +66,7 @@ public class SimpleLinkedListTest {
 
     @Test(expected = NullPointerException.class)
     public void remove_exception() {
-        SimpleLinkedList<Integer> simpleLinkedList = new SimpleLinkedList<>();
+        SimpleList<Integer> simpleLinkedList = new SimpleLinkedList<>();
         simpleLinkedList.add(10);
         simpleLinkedList.add(20);
         simpleLinkedList.add(30);
@@ -66,11 +81,27 @@ public class SimpleLinkedListTest {
         simpleLinkedList.add(20);
         simpleLinkedList.add(30);
         simpleLinkedList.removeLast();
-        LinkNode<Integer> node=simpleLinkedList.get(1);
-        assertThat(node.data,is(20));
+        LinkNode<Integer> node = simpleLinkedList.getNode(1);
+        assertThat(node.data, is(20));
         assertThat(node.next, Matchers.nullValue());
     }
 
+    @Test
+    public void remove_object() {
+        SimpleLinkedList<Singer> simpleLinkedList = new SimpleLinkedList<>();
+        simpleLinkedList.add(new Singer("arijit"));
+        simpleLinkedList.add(new Singer("amal"));
+        simpleLinkedList.add(new Singer("arman"));
+        simpleLinkedList.add(new Singer("neha Kakkar"));
+
+        simpleLinkedList.remove(new Singer("arman"));
+        LinkNode<Singer> node = simpleLinkedList.getNode(2);
+        System.out.println(simpleLinkedList);
+      assertThat(simpleLinkedList.get(0).getName(),is("arijit"));
+      assertThat(simpleLinkedList.get(1).getName(),is("amal"));
+      assertThat(simpleLinkedList.get(2).getName(),is("neha Kakkar"));
+        //getnode.next
+    }
     @Test
     public void remove_position() {
         SimpleLinkedList<Integer> simpleLinkedList = new SimpleLinkedList<>();
@@ -81,15 +112,60 @@ public class SimpleLinkedListTest {
         simpleLinkedList.add(50);
         simpleLinkedList.add(60);
         simpleLinkedList.remove(3);
-       // LinkNode<Integer> node=simpleLinkedLi
+        // LinkNode<Integer> node=simpleLinkedLi
         // st.get();
-        assertThat(simpleLinkedList.get(0).data,is(10));
-        assertThat(simpleLinkedList.get(1).data,is(20));
-        assertThat(simpleLinkedList.get(2).data,is(30));
-        assertThat(simpleLinkedList.get(3).data,is(50));
-        assertThat(simpleLinkedList.get(4).data,is(60));
+        assertThat(simpleLinkedList.get(0) ,is(10));
+        assertThat(simpleLinkedList.get(1), is(20));
+        assertThat(simpleLinkedList.get(2), is(30));
+        assertThat(simpleLinkedList.get(3), is(50));
+        assertThat(simpleLinkedList.get(4), is(60));
 
-       // assertThat(node.data,is(60));
+        // assertThat(node.data,is(60));
         //assertThat(node.next, Matchers.nullValue());
+    }
+    @Test
+    public void add_position() {
+        SimpleLinkedList<Integer> simpleLinkedList = new SimpleLinkedList<>();
+        simpleLinkedList.add(10);
+        simpleLinkedList.add(20);
+        simpleLinkedList.add(30);
+        simpleLinkedList.add(50);
+        simpleLinkedList.add(2,40);
+        System.out.println(simpleLinkedList);
+        assertThat(simpleLinkedList.get(0), is(10));
+        assertThat(simpleLinkedList.get(1), is(20));
+        assertThat(simpleLinkedList.get(2), is(40));
+        assertThat(simpleLinkedList.get(3), is(30));
+        assertThat(simpleLinkedList.get(4), is(50));
+    }
+    @Test
+    public void add_positionAtZero() {
+        SimpleLinkedList<Integer> simpleLinkedList = new SimpleLinkedList<>();
+        simpleLinkedList.add(10);
+        simpleLinkedList.add(20);
+        simpleLinkedList.add(30);
+        simpleLinkedList.add(50);
+        simpleLinkedList.add(0,40);
+        System.out.println(simpleLinkedList);
+        assertThat(simpleLinkedList.get(0), is(40));
+        assertThat(simpleLinkedList.get(1), is(10));
+        assertThat(simpleLinkedList.get(2), is(20));
+        assertThat(simpleLinkedList.get(3), is(30));
+        assertThat(simpleLinkedList.get(4), is(50));
+    }
+    @Test
+    public void add_positionAtOne() {
+        SimpleLinkedList<Integer> simpleLinkedList = new SimpleLinkedList<>();
+        simpleLinkedList.add(10);
+        simpleLinkedList.add(20);
+        simpleLinkedList.add(30);
+        simpleLinkedList.add(50);
+        simpleLinkedList.add(1,40);
+        System.out.println(simpleLinkedList);
+        assertThat(simpleLinkedList.get(0), is(10));
+        assertThat(simpleLinkedList.get(1), is(40));
+        assertThat(simpleLinkedList.get(2), is(20));
+        assertThat(simpleLinkedList.get(3), is(30));
+        assertThat(simpleLinkedList.get(4), is(50));
     }
 }
